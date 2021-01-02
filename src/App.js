c6055985eb5888a1fd6daaf6b9679621a89eb3de
shimotsu4431 <shimotsu13@gmail.com>
2021-01-02 11:57:22 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+import './App.css'
 
-function App() {
+function EditorApp() {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper"><Editor editorState={editorState} onChange={setEditorState} /></div>
   );
 }
 
-export default App;
+export default EditorApp;
