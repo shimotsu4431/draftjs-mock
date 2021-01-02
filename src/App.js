8aefc4946a3bc9fe1afb8324d873908ce35f525e
shimotsu4431 <shimotsu13@gmail.com>
2021-01-02 13:25:10 +0900

    初期値

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, convertFromRaw} from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './App.css'
 
+const initData = convertFromRaw({
+  entityMap: {},
+  blocks: [
+    {
+      key: "xxxxxx",
+      text: "ここに初期テキストがはいります。",
+      type: "unstyled",
+      depth: 0,
+      entityRanges: [],
+      inlineStyleRanges: [],
+      data: {},
+    },
+  ],
+})
+
+const emptyData = EditorState.createWithContent(
+  initData,
+)
+
 function EditorApp() {
-  const [editorState, setEditorState] = useState(
-    () => EditorState.createEmpty(),
-  );
+  const [editorState, setEditorState] = useState(emptyData);
 
   return (
-    <div className="wrapper"><Editor editorState={editorState} onChange={setEditorState} readOnly={true}/></div>
+    <div className="wrapper"><Editor editorState={editorState} onChange={setEditorState} /></div>
   );
 }
 
