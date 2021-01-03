b02be9c35a69a16eb3450e7fa33384ba3dcc8a82
shimotsu4431 <shimotsu13@gmail.com>
2021-01-03 11:25:19 +0900

    add images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,101 @@
 import React, { useState } from "react";
-import { Editor, EditorState} from 'draft-js';
+import { convertFromRaw, Editor, EditorState} from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './App.css'
 
+const initData = convertFromRaw({
+  blocks: [
+    {
+      key: "16d03",
+      text: "なんでもないテキスト。",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "98pea",
+      text: "https://dummyimage.com/100x100/000/fff",
+      type: "atomic",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [
+        {
+          offset: 0,
+          length: 1,
+          key: 0,
+        },
+      ],
+      data: {},
+    },
+    {
+      key: "16d04",
+      text: "なんでもないテキスト。。。。。",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "98peb",
+      text: "https://dummyimage.com/100x100/fff/000",
+      type: "atomic",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [
+        {
+          offset: 0,
+          length: 1,
+          key: 1,
+        },
+      ],
+      data: {},
+    },
+  ],
+  entityMap: {
+    0: {
+      type: "image",
+      mutability: "IMMUTABLE",
+      data: { src: "https://dummyimage.com/100x100/000/fff" },
+    },
+    1: {
+      type: "image",
+      mutability: "IMMUTABLE",
+      data: { src: "https://dummyimage.com/100x100/fff/000" },
+    },
+  },
+});
+
+const initState = EditorState.createWithContent(initData)
+
 function EditorApp() {
-  const [editorState, setEditorState] = useState(
-    () => EditorState.createEmpty(),
-  );
+  const [editorState, setEditorState] = useState(initState);
+
+  const Image = (props) => {
+    return <img src={props.src} alt="" />;
+  };
+
+  const Media = (props) => {
+    const entity = props.contentState.getEntity(props.block.getEntityAt(0));
+    const { src } = entity.getData();
+    const type = entity.getType();
+
+    let media;
+    if (type === "image") {
+      media = <Image src={src} />;
+    }
+
+    return media;
+  };
 
   const myBlockRenderer = (block) => {
-    if (block.getType() === "blockquote") {
+    if (block.getType() === "atomic") {
       return {
+        component: Media,
         editable: false,
-      }
+      };
     }
 
     return null
